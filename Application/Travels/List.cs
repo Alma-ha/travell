72dcd37b50bc81e52cc2dfa9af27fd9597f86a86
Artina Bektashi <ab51034@ubt-uni.net>
2022-05-01 02:35:40 +0200

    comm

--- Application/Travels/List.cs
+++ Application/Travels/List.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Travels
@@ -17,14 +18,16 @@ namespace Application.Travels
         public class Handler : IRequestHandler<Query, List<Travel>>
         {
         private readonly DataContext _context;
+        private readonly ILogger<List> _logger;
             public Handler(DataContext context)
             {
+          
             _context = context;
             }
 
             public async Task<List<Travel>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await _context.Travels.ToListAsync();
+                return await _context.Travels.ToListAsync(cancellationToken);
             }
         }
     }
